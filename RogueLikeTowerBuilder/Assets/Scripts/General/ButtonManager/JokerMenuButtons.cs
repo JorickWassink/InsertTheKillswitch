6942f62b801ff699f260fdb7f532fa939d03037e
Jorick Wassink <wassinkjorick@gmail.com>
2026-06-02 10:47:56 +0200

    made dll menu

---
+++ RogueLikeTowerBuilder/Assets/Scripts/General/ButtonManager/JokerMenuButtons.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class JokerMenuButtons : MonoBehaviour
+{
+    [SerializeField] Transform openTransform;
+    [SerializeField] Transform closedTransform;
+
+    GameObject target;
+    Transform currentTarget = null;
+    private void Start()
+    {
+        target = FindAnyObjectByType<JokerMenu>().gameObject;
+    }
+    public void ToggleMenu()
+    {
+        currentTarget = (currentTarget == openTransform) ? closedTransform : openTransform;
+    }
+
+    private void MoveMenu()
+    {
+        if (target.transform == currentTarget || currentTarget == null) return;
+
+        target.transform.position = Vector3.MoveTowards(target.transform.position, currentTarget.position, .1f);
+
+    }
+
+    private void FixedUpdate()
+    {
+        MoveMenu();
+    }
+}
