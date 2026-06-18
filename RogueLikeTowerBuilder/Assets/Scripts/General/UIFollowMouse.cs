1a187c3e58e61b7c914230bb3a90ccb3e9b208fd
Jorick Wassink <wassinkjorick@gmail.com>
2026-06-18 13:08:52 +0200

    feat: changed dll menu

---
+++ RogueLikeTowerBuilder/Assets/Scripts/General/UIFollowMouse.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIFollowMouse : MonoBehaviour
+{
+    //public RectTransform canvasRect;
+    //public RectTransform tooltipRect;
+    //public Camera uiCamera;
+
+    //private void Start()
+    //{
+    //    canvasRect = FindAnyObjectByType<MainCanvas>().GetComponent<RectTransform>();
+    //    tooltipRect = GetComponent<RectTransform>();
+    //    uiCamera = Camera.main;
+    //}
+
+    //void Update()
+    //{
+    //    Vector2 mousePos = Input.mousePosition;
+
+    //    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+    //        canvasRect,
+    //        mousePos,
+    //        uiCamera,
+    //        out Vector2 localPoint
+    //    );
+
+    //    localPoint.x -= 5;
+    //    localPoint.y -= 5;
+
+    //    tooltipRect.localPosition = localPoint;
+    //}
+}
